d1c0fecc428fdba95d9c2fefbe959c3fba60400f
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-16 02:18:33 -0400

    API With JWT, JOI, and bcrypt done

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,15 @@ const { route } = require("./auth");
 
 const router = require("express").Router();
 const privateRoute = require("./privateRoute");
+const User = require("../models/User");
 
-router.get("/", privateRoute, (req, res) => {
-  res.json({
-    posts: { title: "First post attempt", description: "random data" },
-  });
+router.get("/", privateRoute, async (req, res) => {
+  //   res.json({
+  //     posts: { title: "First post attempt", description: "random data" },
+  //   });
+  //   res.send(req.user);
+  const user = await User.findById(req.user._id);
+  res.send(user);
 });
 
 module.exports = router;
